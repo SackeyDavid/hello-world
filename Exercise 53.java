d366402f48b5d57b6f8ddfe0031eea7a9d0f2b52
SackeyDavid <davidkofiahensackey@gmail.com>
2016-03-18 10:08:04 +0000

    Assignment 2 uploaded
    NumberGuessingWithACounter.java

---
+++ Exercise 53.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignmentii;
+
+/**
+ *
+ * @author David
+ */
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingWithCounter {
+
+public static void main(String[] args)
+{
+	Scanner in = new Scanner(System.in);
+	Random r = new Random();
+
+	int x = 1 + r.nextInt(10);
+	int guess;
+	int counter = 0;
+ 	System.out.println("I have chosen a number between 1 and 10. Try to guess it.");
+	System.out.print("Your guess: ");
+	guess  = in.nextInt();
+	counter ++;
+
+	while (guess != x )
+	{
+	System.out.println("That is incorrect. Guess again.");
+	System.out.print("Your guess: ");
+	guess  = in.nextInt();
+	counter ++;
+	}
+	System.out.println("That's right! You're a good guesser.");
+	System.out.println("It only took you " + counter + " tries.");
+}
+}
+
